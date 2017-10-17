b229f24e6ee11265d61c3293523b78f41cd68385
Ignacio Cicero <ignaciocicero@gmail.com>
2017-10-17 20:33:17 -0300

    first approach to dynamic content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+var app = angular.module("contents", []); 
+app.controller("contentsController", function($scope, $http) {
+  $http.get("contents.json").then(function(response) {
+    console.log(response);
+  });
+});
