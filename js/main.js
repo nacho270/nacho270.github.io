4c0d26d0de0472160c5ab15615381aed096cde81
Ignacio Cicero <ignaciocicero@gmail.com>
2017-10-17 23:50:42 -0300

    Multiple entries with react + new navbar color

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-var Entry =  React.createClass({
-  render: function() {
-    return (
-      <div className="col-md-4">
-        <h2>{this.props.title}</h2>
-        <p>{this.props.body}</p>
-        <p><a className="btn btn-default" href={this.props.link} target="_blank" role="button">View details &raquo;</a></p>
-      </div>
-    )
-  }
-});
-
 var Footer =  React.createClass({
   render: function() {
     return (
@@ -23,37 +11,87 @@ var Footer =  React.createClass({
   }
 });
 
-var Contents = React.createClass({
+var Entry =  React.createClass({
+  render: function() {
+    return (
+      <div className="col-md-4">
+        <h2>{this.props.title}</h2>
+        <p>{this.props.body}</p>
+        <p><a className="btn btn-default" href={this.props.link} target="_blank" role="button">View details &raquo;</a></p>
+      </div>
+    )
+  }
+});
+
+var Row = React.createClass({
+  render : function () {
+    return (
+      <div className="row">
+        {this.props.entries}
+      </div>
+    );
+  }
+});
+
+var RowContainer = React.createClass({
+  render : function () {
+    return (
+      <div>
+        {this.props.rows}
+      </div>
+    );
+  }
+});
+
+var Container = React.createClass({
   getInitialState: function() {
     return {
-      content: []
+      entries: []
     }
   },
   componentDidMount() {
     let oThis = this;
     var m = new Date().getTime();
     $.get("contents.json?m=" + m, function(data, status){
-      oThis.setState({ content: data });
+      oThis.setState({ entries: data });
     });
   },
   render: function() {
-    if(this.state.content.length >0) {
-      let content = this.state.content;
-      return (
-        <div className="row">
-          <Entry title={this.state.content[0].title}
-          body={this.state.content[0].body}
-          link={this.state.content[0].link} />
-          <Footer />
-        </div>
-      );
+    if(this.state.entries.length == 0) {
+      return(<div><RowContainer rows={[]}/> <Footer /></div>)
     }
-    <Footer />
-    return(<div className="row"><Footer /></div>)
+
+    let entries = this.state.entries;
+    let rows = {};
+    let rowIndex = 0;
+    rows[rowIndex] = {}
+    rows[rowIndex].entries = [];
+    for(let i in entries) {
+      if(i> 0 && i % 3 === 0) {
+        rowIndex++;
+        rows[rowIndex] = {}
+        rows[rowIndex].entries = [];
+      }
+      rows[rowIndex].entries.push(<Entry key={i}
+                                          title={entries[i].title}
+                                          body={entries[i].body}
+                                          link={entries[i].link} />)
+    }
+    let actualRows = [];
+    for (let i in rows) {
+      actualRows.push(<Row key={i} entries={rows[i].entries} />);
+    }
+    console.log(actualRows[0].addAlgo);
+    return (
+      <div>
+        <RowContainer rows={actualRows}/>
+        <Footer />
+      </div>
+    );
   }
 });
 
 ReactDOM.render(
-  <Contents />,
+  <Container />,
   document.getElementById('content')
 );
