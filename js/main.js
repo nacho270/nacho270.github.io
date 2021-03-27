8c8a60109766817a5c3889f70a7eb9e68e964aa4
Ignacio Cicero <ignaciocicero@gmail.com>
2021-03-27 17:48:24 +0000

    Added medium articles

--- js/main.js
+++ js/main.js
@@ -1,37 +1,42 @@
-var app = angular.module("contents",  ['ngSanitize']);
-app.controller("contentsController", function($scope, $http) {
-    let rand = new Date().getTime();
-    $http.get(`contents.json?r=${rand}`).then(function(response) {
-        $scope.entries = response.data;
+var app = angular.module("contents", ["ngSanitize"]);
+app.controller("contentsController", function ($scope, $http) {
+  let rand = new Date().getTime();
+  $http.get(`contents.json?r=${rand}`).then(function (response) {
+    $scope.entries = response.data;
+  });
+  $http.get(`cv.json?r=${rand}`).then(function (response) {
+    $scope.cv = response.data;
+  });
+  $scope.changeModal = function (title, body) {
+    $("#myModal .modal-title").html(title);
+    $("#myModal .modal-body").html(body);
+    $("#myModal pre, #myModal code").each(function (i, block) {
+      hljs.highlightBlock(block);
     });
-    $http.get(`cv.json?r=${rand}`).then(function(response) {
-        $scope.cv = response.data;
-    });
-    $scope.changeModal = function(title, body){
-        $("#myModal .modal-title").html(title);
-        $("#myModal .modal-body").html(body);
-        $('#myModal pre, #myModal code').each(function(i, block) {
-            hljs.highlightBlock(block);
-          });
-      };
+  };
 });
 
-app.filter('cut', function () {
-    return function (value, wordwise, max, tail) {
-        if (!value) return '';
-        max = parseInt(max, 10);
-        if (!max) return value;
-        if (value.length <= max) return value;
-        value = value.substr(0, max);
-        if (wordwise) {
-            var lastspace = value.lastIndexOf(' ');
-            if (lastspace !== -1) {
-              if (value.charAt(lastspace-1) === '.' || value.charAt(lastspace-1) === ',') {
-                lastspace = lastspace - 1;
-              }
-              value = value.substr(0, lastspace);
-            }
+app.filter("cut", function () {
+  return function (value, wordwise, max, tail) {
+    max = parseInt(max, 10);
+    if (!value) return "";
+    if (!max) return value;
+    if (value.length <= max) return value;
+
+    value = value.substr(0, max);
+
+    if (wordwise) {
+      var lastspace = value.lastIndexOf(" ");
+      if (lastspace !== -1) {
+        if (
+          value.charAt(lastspace - 1) === "." ||
+          value.charAt(lastspace - 1) === ","
+        ) {
+          lastspace = lastspace - 1;
         }
-        return value + (tail || ' …');
-    };
+        value = value.substr(0, lastspace);
+      }
+    }
+    return value + (tail || " …");
+  };
 });
